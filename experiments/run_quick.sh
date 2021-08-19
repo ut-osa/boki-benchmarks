864c472cb21ddcd5500ac90b2d2ea30a17528038
Zhipeng Jia <zjia@cs.utexas.edu>
2021-08-19 02:41:26 +0000

    The push-button script

--- experiments/run_quick.sh
+++ experiments/run_quick.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 ROOT_DIR=`realpath $(dirname $0)/..`
 
+# Message queue workload for BokiQueue and Pulsar
+RUN_QUEUE_BOKI=y
+RUN_QUEUE_PUSLAR=y
+
+# Retwis workload for BokiStore and MongoDB
+RUN_STORE_BOKI=y
+RUN_STORE_MONGO=y
+
+# Workflow workload for BokiFlow and Beldi
+RUN_WORKFLOW_BOKI=y
+RUN_WORKFLOW_BELDI=y
+
 HELPER_SCRIPT=$ROOT_DIR/scripts/exp_helper
 
 # This IAM role has DynamoDB read/write access
 BOKI_MACHINE_IAM=boki-ae-experiments
 
-
+if [[ ! -z $RUN_QUEUE_BOKI ]] && [[ $RUN_QUEUE_BOKI == "y" ]]; then
 echo "====== Start running BokiQueue experiments ======"
 
 BASE_DIR=$ROOT_DIR/experiments/queue/boki
@@ -20,9 +32,13 @@ $BASE_DIR/run_once.sh p32c128  128 3 1 32
 $HELPER_SCRIPT stop-machines --base-dir=$BASE_DIR
 
 echo "====== Finish running BokiQueue experiments ======"
+else
+echo "====== Skip BokiQueue experiments ======"
+fi
 echo ""
 
 
+if [[ ! -z $RUN_QUEUE_PUSLAR ]] && [[ $RUN_QUEUE_PUSLAR == "y" ]]; then
 echo "====== Start running Pulsar experiments ======"
 
 BASE_DIR=$ROOT_DIR/experiments/queue/pulsar
@@ -36,9 +52,51 @@ $BASE_DIR/run_once.sh p32c128  3 32  128
 $HELPER_SCRIPT stop-machines --base-dir=$BASE_DIR
 
 echo "====== Finish running Pulsar experiments ======"
+else
+echo "====== Skip Pulsar experiments ======"
+fi
 echo ""
 
 
+if [[ ! -z $RUN_STORE_BOKI ]] && [[ $RUN_STORE_BOKI == "y" ]]; then
+echo "====== Start running BokiStore experiments ======"
+
+BASE_DIR=$ROOT_DIR/experiments/retwis/boki
+
+$HELPER_SCRIPT start-machines --base-dir=$BASE_DIR --instance-iam-role $BOKI_MACHINE_IAM
+
+$BASE_DIR/run_once.sh con128 128
+$BASE_DIR/run_once.sh con192 192
+
+$HELPER_SCRIPT stop-machines --base-dir=$BASE_DIR
+
+echo "====== Finish running BokiStore experiments ======"
+else
+echo "====== Skip BokiStore experiments ======"
+fi
+echo ""
+
+
+if [[ ! -z $RUN_STORE_MONGO ]] && [[ $RUN_STORE_MONGO == "y" ]]; then
+echo "====== Start running MongoDB experiments ======"
+
+BASE_DIR=$ROOT_DIR/experiments/retwis/mongodb
+
+$HELPER_SCRIPT start-machines --base-dir=$BASE_DIR --instance-iam-role $BOKI_MACHINE_IAM
+
+$BASE_DIR/run_once.sh con128 128
+$BASE_DIR/run_once.sh con192 192
+
+$HELPER_SCRIPT stop-machines --base-dir=$BASE_DIR
+
+echo "====== Finish running MongoDB experiments ======"
+else
+echo "====== Skip MongoDB experiments ======"
+fi
+echo ""
+
+
+if [[ ! -z $RUN_WORKFLOW_BOKI ]] && [[ $RUN_WORKFLOW_BOKI == "y" ]]; then
 echo "====== Start running BokiFlow experiments ======"
 
 BASE_DIR=$ROOT_DIR/experiments/workflow/boki-hotel
@@ -62,9 +120,13 @@ $BASE_DIR/run_once.sh qps150 150
 $HELPER_SCRIPT stop-machines --base-dir=$BASE_DIR
 
 echo "====== Finish running BokiFlow experiments ======"
+else
+echo "====== Skip BokiFlow experiments ======"
+fi
 echo ""
 
 
+if [[ ! -z $RUN_WORKFLOW_BELDI ]] && [[ $RUN_WORKFLOW_BELDI == "y" ]]; then
 echo "====== Start running Beldi experiments ======"
 
 BASE_DIR=$ROOT_DIR/experiments/workflow/beldi-hotel
@@ -88,4 +150,7 @@ $BASE_DIR/run_once.sh qps150 150
 $HELPER_SCRIPT stop-machines --base-dir=$BASE_DIR
 
 echo "====== Finish running Beldi experiments ======"
+else
+echo "====== Skip Beldi experiments ======"
+fi
 echo ""
